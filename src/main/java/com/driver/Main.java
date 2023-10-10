897c2de5847364972cd402ae709aa49751c45f3f
Ayush63068 <101062754+Ayush63068@users.noreply.github.com>
2023-10-10 07:23:22 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
+class Product{
 
+ public int Product(int x,int y){}
+  public int product(int x, int y, int z) {}
+   public double product(double x, double y) {}
+}
+  public static void main(String[]args){
+    Product p= new Product();
+    p.Product(1,2);
+    p.Product(1,2,3);
+    p.Product(2.0,3.0);
+  }
 }
